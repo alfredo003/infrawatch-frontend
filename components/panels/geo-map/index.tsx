9e509dee60442dc06e1e236b3eda365f7c0366ee
Fabio Nicolau Domingos <fnicolau@student.42luanda.com>
2025-09-04 19:27:23 +0100

    refactor: enhance GeoMap component layout and add status cards

--- components/panels/geo-map/index.tsx
+++ components/panels/geo-map/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import dynamic from "next/dynamic";
 import { cn } from "@/lib/utils";
 import {
@@ -9,7 +9,15 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  PackageCheck,
+  AlertTriangle,
+  Plus,
+  Settings,
+  MapPinned,
+} from "lucide-react";
 
 const Map = dynamic(() => import("./partials/Map"), { ssr: false });
 
@@ -58,8 +66,83 @@ export default function GeoMap() {
   }
 
   return (
-    <main className="relative z-0 h-full w-full">
-      <Map onSelectMachine={openSheet} />
+    <main className="relative z-0 h-screen w-full bg-white dark:bg-black text-black dark:text-white flex flex-col gap-4 p-6">
+      <section className="">
+        <div className="flex justify-between items-center mb-8">
+          <div>
+            <h1 className="text-3xl font-bold text-blue-600 dark:text-blue-400">
+              Localizações geográficas
+            </h1>
+            <p className="text-neutral-600 dark:text-neutral-400 mt-2">
+              Localize as máquinas no mapa
+            </p>
+          </div>
+          <Button
+            // onClick={() => alert()}
+            className="bg-blue-600 text-white hover:bg-blue-600/70 cursor-pointer"
+          >
+            <Plus className="w-4 h-4" />
+          </Button>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <Card>
+            <CardContent className="p-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                    Total mapeadas
+                  </p>
+                  <p className="text-2xl font-bold text-blue-600">7</p>
+                </div>
+                <MapPinned className="w-8 h-8 text-blue-600" />
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="p-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                    Em manutenção
+                  </p>
+                  <p className="text-2xl font-bold text-orange-600">5</p>
+                </div>
+                <Settings className="w-8 h-8 text-orange-600" />
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="p-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                    Operacionais
+                  </p>
+                  <p className="text-2xl font-bold text-green-600">4</p>
+                </div>
+                <PackageCheck className="w-8 h-8 text-green-600" />
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="p-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                    Com Problemas
+                  </p>
+                  <p className="text-2xl font-bold text-red-600">5</p>
+                </div>
+                <AlertTriangle className="w-8 h-8 text-red-600" />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+
+      <section className="w-full flex-1 rounded-md">
+        <Map onSelectMachine={openSheet} />
+      </section>
       {selectedMachine && (
         <Sheet open={open} onOpenChange={setOpen}>
           <SheetContent side="right" className="w-[420px] overflow-auto p-6">
@@ -68,14 +151,15 @@ export default function GeoMap() {
                 <SheetTitle className="text-2xl font-semibold">
                   {selectedMachine.name}
                 </SheetTitle>
-                <Badge
+
+                <span
                   className={cn(
-                    "rounded-full px-3 py-1 text-xs font-semibold",
+                    "rounded-full px-2 py-0.5 text-[10px] font-medium",
                     machineStatus[selectedMachine.status.id]
                   )}
                 >
                   {selectedMachine.status.label}
-                </Badge>
+                </span>
               </div>
             </SheetHeader>
 
